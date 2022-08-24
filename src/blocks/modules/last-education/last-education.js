5fe694cd26aebb3540010cf639612eba0908ae1f
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2022-08-24 15:21:30 +0300

    save

---
+++ src/blocks/modules/last-education/last-education.js
@@ -0,0 +1,13 @@
+modules.define('last-education', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/last-medicine/last-medicine.js
@@ -0,0 +1,13 @@
+modules.define('last-medicine', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
